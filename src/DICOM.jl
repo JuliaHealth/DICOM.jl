091e683e3d36cd725b3848ab0e463dcc0b5ded45
Zaki A <zaki@live.ca>
2020-08-03 00:28:42 -0400

    Remove syntax that doesn't exist in Julia 1.0

--- src/DICOM.jl
+++ src/DICOM.jl
@@ -130,7 +130,10 @@ always_implicit(grp, elt) =
     (grp == 0xFFFE && (elt == 0xE0DD || elt == 0xE000 || elt == 0xE00D))
 
 function find_dicom_files(dir; join=true)
-    files = readdir(dir; join)
+    files = readdir(dir)
+    if join
+        @. files = joinpath(dir, files)
+    end
     dicom_files = filter(file -> isdicom(file), files)
     return dicom_files
 end
